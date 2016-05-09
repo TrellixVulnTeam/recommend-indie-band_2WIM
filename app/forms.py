7ea434c4053a6482826178d0d1b2a0a68c853d4b
dschuler36 <dschuler36@gmail.com>
2016-05-09 14:40:45 -0400

    fixed error handling

--- app/forms.py
+++ app/forms.py
@@ -4,16 +4,16 @@ import config
 
 
 class ArtistsForm(Form):
-    artist1 = TextField('Artist 1', [validators.Required(
-        "Please enter an artist for each field")])
-    artist2 = TextField('Artist 2', [validators.Required(
-        "Please enter an artist for each field")])
-    artist3 = TextField('Artist 3', [validators.Required(
-        "Please enter an artist for each field")])
-    artist4 = TextField('Artist 4', [validators.Required(
-        "Please enter an artist for each field")])
-    artist5 = TextField('Artist 5', [validators.Required(
-        "Please enter an artist for each field")])
+    artist1 = TextField(validators.Required(
+        "Please enter an artist for each field"))
+    artist2 = TextField(validators.Required(
+        "Please enter an artist for each field"))
+    artist3 = TextField(validators.Required(
+        "Please enter an artist for each field"))
+    artist4 = TextField(validators.Required(
+        "Please enter an artist for each field"))
+    artist5 = TextField(validators.Required(
+        "Please enter an artist for each field"))
 
 '''
 class HiddenForm(Form):
--- app/static/js/validate.js
+++ app/static/js/validate.js
@@ -1,49 +1,50 @@
-$(document).ready(function() {
-  $("#firstButton").on("click", function() {
-    artists = []
-    var artist1 = $("#artist1").val();
-    artists.push(artist1);
-    var artist2 = $("#artist2").val();
-    artists.push(artist2);
-    var artist3 = $("#artist3").val();
-    artists.push(artist3);
-    var artist4 = $("#artist4").val();
-    artists.push(artist4);
-    var artist5 = $("#artist5").val();
-    artists.push(artist5);
-
-    i = 1;
-    fixedArtists = []
-    artists.forEach(function(artist) {
-      console.log(artist);
-      validate(artist, i);
-      i++;
-    })
-
-  function validate(artist, i){
-    $.ajax({
-      type: 'POST',
-      url: 'http://ws.audioscrobbler.com/2.0/',
-      data: 'method=artist.getinfo' +
-        '&artist=' + artist +
-        '&autocorrect=1' +
-        '&api_key=bd7e293a51b53716d502db4d74835f01' +
-        '&format=json',
-      success: function(data) {
-        var fixedArtist = data.artist.name;
-        console.log(fixedArtist);
-        $('#artist'+i).val(fixedArtist);
-        $('#hidden'+i).val(fixedArtist);
-        if (i == 5) {
-          $('#secondButton').click();
-          console.log('button clicked');
-        }
-      },
-      error: function(code, message) {
-        console.log('Error: ', message);
-        $('#artist'+i).val();
-      }
-    });
-  }
+$("#firstButton").on('click', function(e) {
 });
+$("#secondButton").on("click", function(e) {
+  artists = []
+  var artist1 = $("#artist1").val();
+  artists.push(artist1);
+  var artist2 = $("#artist2").val();
+  artists.push(artist2);
+  var artist3 = $("#artist3").val();
+  artists.push(artist3);
+  var artist4 = $("#artist4").val();
+  artists.push(artist4);
+  var artist5 = $("#artist5").val();
+  artists.push(artist5);
+
+  i = 1;
+  fixedArtists = []
+  artists.forEach(function(artist) {
+    if (artist == undefined) {
+      alert('Please be sure to enter an artist for each field.');
+    };
+    validate(artist, i);
+    i++;
+  });
+
+function validate(artist, i){
+  $.ajax({
+    type: 'POST',
+    async: false,
+    url: 'http://ws.audioscrobbler.com/2.0/',
+    data: 'method=artist.getinfo' +
+      '&artist=' + artist +
+      '&autocorrect=1' +
+      '&api_key=bd7e293a51b53716d502db4d74835f01' +
+      '&format=json',
+    success: function(data) {
+      var fixedArtist = data.artist.name;
+      $('#hidden'+i).val(fixedArtist);
+      $('#artist'+i).val(fixedArtist);
+      if (i == 5) {
+        $('#firstButton').click();
+      }
+    },
+    error: function(code, message) {
+      console.log('Error: ', message);
+      $('#artist'+i).val();
+    }
+  });
+};
 });
