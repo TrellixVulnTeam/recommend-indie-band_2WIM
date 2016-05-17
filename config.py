b3942556973c6a27f14811ced9c54f6d5ba11b8b
dschuler36 <dschuler36@gmail.com>
2016-05-17 15:23:42 -0400

    update redis con

--- config.py
+++ config.py
@@ -16,6 +16,6 @@ API_SECRET = "b11413d5d68dad40029e06d1d33f753f"
 username = "dschuler36"
 password_hash = pylast.md5("s002443")
 
-redis_url = os.getenv('REDIS_URL', 'redis://h:p7fb6tbeko7o499k2ric5bpq9lf@ec2-54-227-250-222.compute-1.amazonaws.com:9809')
-redis_port = 9809
-redis_pass = 'p7fb6tbeko7o499k2ric5bpq9lf'
+redis_url = os.getenv('REDIS_URL', 'redis://redistogo:61f64edfbf63e3009926b41556e83aee@catfish.redistogo.com:10342/')
+redis_port = 10342
+redis_pass = '61f64edfbf63e3009926b41556e83aee'
