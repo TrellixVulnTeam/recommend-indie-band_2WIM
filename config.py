02435d886ca010c402521c17c143a90ad084d953
dschuler36 <dschuler36@gmail.com>
2016-05-18 12:37:49 -0400

    update

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import pylast
 import os
-import redis
+
 
 
 WTF_CSRF_ENABLED = True
@@ -15,8 +15,3 @@ API_KEY = "bd7e293a51b53716d502db4d74835f01"
 API_SECRET = "b11413d5d68dad40029e06d1d33f753f"
 username = "dschuler36"
 password_hash = pylast.md5("s002443")
-
-redis_url = os.getenv('REDISTOGO_URL',
-                      'redis://redistogo:61f64edfbf63e3009926b41556e83aee@catfish.redistogo.com:10342/')
-redis_port = 10342
-redis_pass = '61f64edfbf63e3009926b41556e83aee'
